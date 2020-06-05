057966bab6c8b90fb97f020b14607f619846d151
Saurabh Ranjan <inrsaurabh@gmail.com>
2020-06-05 14:27:21 +0530

    Updates

---
+++ precache-manifest.9e8bc07ad340847892606256801c51b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef901201e37b3a095aa889dc71498147",
+    "url": "/html2canvas/index.html"
+  },
+  {
+    "revision": "45eb31304d78498ccd06",
+    "url": "/html2canvas/static/css/main.cae9c554.chunk.css"
+  },
+  {
+    "revision": "32c7333ac455ec3d6c32",
+    "url": "/html2canvas/static/js/2.e7dd5358.chunk.js"
+  },
+  {
+    "revision": "7a096c7570400737bb7dfc314590a37e",
+    "url": "/html2canvas/static/js/2.e7dd5358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45eb31304d78498ccd06",
+    "url": "/html2canvas/static/js/main.423eefa6.chunk.js"
+  },
+  {
+    "revision": "b1d01b5b3b8b37a1df00",
+    "url": "/html2canvas/static/js/runtime-main.cf44c41a.js"
+  }
+]);
